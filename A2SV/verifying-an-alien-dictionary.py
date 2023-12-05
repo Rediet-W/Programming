4d94fb76b96f8daaff62b1025fe06e1b9090840c
Rediet-W <139815109+Rediet-W@users.noreply.github.com>
2023-12-05 16:38:48 +0300

    Added solution for Verifying an Alien Dictionary

---
+++ A2SV/verifying-an-alien-dictionary.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        for i in range(len(words) - 1):
+            for j in range(len(words[i])):
+                if j >= len(words[i+1]):
+                    return False
+                if order.index(words[i][j]) < order.index(words[i+1][j]):
+                    break
+                elif order.index(words[i][j]) > order.index(words[i+1][j]):
+                    return False
+        return True
+

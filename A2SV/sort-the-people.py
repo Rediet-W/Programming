267b6f3fe3b9eab1ad8c00f572634a8f00395f6d
Rediet-W <139815109+Rediet-W@users.noreply.github.com>
2023-12-26 18:51:54 +0300

    Added solution for Sort the People

--- A2SV/sort-the-people.py
+++ A2SV/sort-the-people.py
@@ -1,16 +1,9 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        
-        for i in range(len(heights)):
-            max_idx = i
-            max_val = heights[i]
-            for j in range(i + 1,len(heights)):
-                if max_val < heights[j]:
-                    max_val = heights[j]
-                    max_idx = j
-            heights[i], heights[max_idx] = heights[max_idx], heights[i]
-            names[i], names[max_idx] = names[max_idx] , names[i]
-
+        for j in range(len(heights) - 1):
+            for i in range(len(heights)-j-1):
+                if heights[i] < heights[i+1]:
+                    heights[i], heights[i+1] = heights[i+1] , heights[i]
+                    names[i], names[i+1] = names[i+1], names[i]
         return names
-            
 

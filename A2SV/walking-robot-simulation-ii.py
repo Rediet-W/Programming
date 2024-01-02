8672ce8cabfa7b03c40686e9a4958fb58b175429
Rediet-W <139815109+Rediet-W@users.noreply.github.com>
2024-01-02 14:49:29 +0300

    Added solution for Walking Robot Simulation II

---
+++ A2SV/walking-robot-simulation-ii.py
@@ -0,0 +1,40 @@
+class Robot:
+
+    def __init__(self, width: int, height: int):
+        self.x= 0
+        self.y = 0
+        self.w = width
+        self.h= height
+        self.dir = 0
+        self.directions =[(1,0),(0,1),(-1,0),(0,-1)]
+        self.dlabel = ["East", "North", "West", "South"]
+
+    def step(self, num: int) -> None:
+        num %= (self.w -1 + self.h - 1) * 2
+        if num == 0:
+            num+=(self.w - 1 + self.h -1) *2 #if it is at start position it can loop and return to itself
+        
+        for _ in range(num):
+            nx,ny = self.x + self.directions[self.dir][0], self.y + self.directions[self.dir][1]
+
+            while not(0<= nx < self.w and 0<= ny < self.h):
+                self.dir = (self.dir + 1) % 4
+                nx,ny = self.x + self.directions[self.dir][0], self.y + self.directions[self.dir][1]
+            
+            self.x= nx
+            self.y = ny
+            
+
+    def getPos(self) -> List[int]:
+        return [self.x, self.y]
+
+    def getDir(self) -> str:
+        return self.dlabel[self.dir]
+        
+
+
+# Your Robot object will be instantiated and called as such:
+# obj = Robot(width, height)
+# obj.step(num)
+# param_2 = obj.getPos()
+# param_3 = obj.getDir()

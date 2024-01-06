f90c9b908e03ae5336e144654369cfcd9a9de6e3
Rediet-W <139815109+Rediet-W@users.noreply.github.com>
2024-01-06 03:40:25 +0300

    Added solution for Minimum Processing Time

---
+++ A2SV/minimum-processing-time.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minProcessingTime(self, processorTime: List[int], tasks: List[int]) -> int:
+        tasks.sort(reverse=True)
+        processorTime.sort()
+        mx=0        
+        for i in range(len(processorTime)):
+            mx = max(mx, processorTime[i] + max(tasks[i*4: i*4+4]))
+        return mx

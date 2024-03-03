8387ef4c302bc5879f9cc7e52bd79f3c6eee9910
Rediet-W <139815109+Rediet-W@users.noreply.github.com>
2024-03-03 12:11:22 +0300

    Add solution for Letter Combinations of a Phone Number

---
+++ leetcode/letter-combinations-of-a-phone-number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        res = []
+        dictt = {"2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
+        def backtrack(i, curr):
+            
+            if len(curr) == len(digits):
+                res.append(curr)
+                return 
+            for j in dictt[digits[i]]:
+
+                backtrack(i+1, curr+j)
+
+        if digits:
+            backtrack(0, "")
+        return res
